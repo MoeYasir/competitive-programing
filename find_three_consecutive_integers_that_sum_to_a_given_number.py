dec9762b627cc05ae198dd9da6e11626bfcd024f
MoeYasir <mohammed-yasir@hotmail.com>
2024-02-13 13:25:31 +0200

    restructure

--- find_three_consecutive_integers_that_sum_to_a_given_number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def sumOfThree(self, num: int) -> List[int]:
-        if num % 3 ==0:
-            num = num//3
-            return [num-1, num, num+1]
-        return []
-
-        
