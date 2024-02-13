d46d435ea73b419f8101a88e7a72179d4911e7a0
Moe <101048907+MoeYasir@users.noreply.github.com>
2024-02-13 13:03:56 +0200

    Create rearrange_array_elements_by_sign.py

---
+++ Python/rearrange_array_elements_by_sign.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        positives = [num for num in nums if num >0]
+        negatives = [num for num in nums if num <0]
+        result = []
+        for i in range(len(nums) //2):
+            result.append(positives[i])
+            result.append(negatives[i])
+        return result
+        
