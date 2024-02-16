5a84fef7f32887731cefd1e16ca27e2a16fcdc8f
Moe <101048907+MoeYasir@users.noreply.github.com>
2024-02-16 20:16:10 +0200

    Create design_an_ordered_stream.py

---
+++ Python/design_an_ordered_stream.py
@@ -0,0 +1,20 @@
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.values = [0 for i in range(n)]
+        self.n = n
+        self.pointer = 0
+        
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.values[idKey-1] = value
+        
+        temp = []
+        while self.pointer < self.n and self.values[self.pointer] != 0:
+            temp.append(self.values[self.pointer])
+            self.pointer += 1
+        return temp
+
+        
+
+
